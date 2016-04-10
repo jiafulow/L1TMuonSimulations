10a997fffa1b412767a34be58ae247c29a7dfd1d
Jia Fu Low <jia.fu.low@cern.ch>
2016-04-10 11:13:22 -0400

    Add more particle gun fragments

---
+++ Configuration/python/SingleMuonFlatPt10_PositiveEndCap_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+# reference: https://github.com/cms-sw/genproductions/blob/master/python/EightTeV/SingleMuMinusFlatPt0p2To100_cff.py
+generator = cms.EDProducer("FlatRandomPtGunProducer2",
+    PGunParameters = cms.PSet(
+        MaxPt = cms.double(10.0*1.001),
+        MinPt = cms.double(10.0),
+        PartID = cms.vint32(-13),
+        MinEta = cms.double(0.9),
+        MaxEta = cms.double(2.5),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        RandomCharge = cms.bool(True),
+        PtSpectrum = cms.string('flatPt'),
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('single muon+/- pt 10 positive endcap'),
+    AddAntiParticle = cms.bool(False),
+    firstRun = cms.untracked.uint32(1)
+)
