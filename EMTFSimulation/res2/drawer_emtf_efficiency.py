934bb5324f968b6cd764225e7d99f59d739b26f8
Jia Fu Low <jia.fu.low@cern.ch>
2016-07-08 17:29:24 -0400

    Update drawer_emtf_efficiency.py [2]

--- EMTFSimulation/res2/drawer_emtf_efficiency.py
+++ EMTFSimulation/res2/drawer_emtf_efficiency.py
@@ -10,15 +10,17 @@ fcol = TColor.GetColor("#a6cee3")  # mu0
 
 l1t_l1pt_vec = [0, 1, 2]  # 3 trigger pt bins
 l1t_l1pt_thresholds = (10, 16, 20)
-l1t_eta_vec = [0, 1, 2, 3, 4]  # 4 eta bins + 1 inclusive eta bin
-l1t_eta_bounds = (1.2, 1.5, 1.8, 2.1, 2.4)
-l1t_pu_vec = [0, 1, 2, 3, 4, 5]  # 6 pu bins
-l1t_pu_bounds = (-5, 5, 15, 25, 35, 45, 55)
+l1t_mode_vec = [0, 1, 2]  # 3-station, 4-station, 3-or-4-station
+l1t_mode_values = ((11,13,14), (15,), (11,13,14,15))
+l1t_eta_vec = [0, 1, 2]  # 2 eta bins + 1 inclusive eta bin
+l1t_eta_bounds = (1.2, 1.6, 2.4)
+l1t_pu_vec = [0, 1, 2, 3]  # 4 pu bins
+l1t_pu_values = (0, 20, 30, 50)
 
 
 # ______________________________________________________________________________
 # Helper
-def get_l1t_l1pt_bins(l1pt):
+def get_l1t_l1pt_bins():
     bins = l1t_l1pt_vec
     return bins
 
@@ -26,13 +28,20 @@ def get_l1t_l1pt_label(l1ptbin):
     label = "L1T p_{T} > %i" % l1t_l1pt_thresholds[l1ptbin]
     return label
 
+def get_l1t_mode_bins():
+    bins = l1t_mode_vec
+    return bins
+
+def get_l1t_mode_label(modebin):
+    myrepr = lambda xx: '['+(','.join([str(x) for x in xx]))+']'
+    label = "mode #in [%s]" % (myrepr(l1t_mode_values[modebin]))
+    return label
+
 def get_l1t_eta_bin(eta):
     eta = abs(eta)
-    if eta < l1t_eta_bounds[0] or eta >= l1t_eta_bounds[-1]:
-        return -1
-    for i in xrange(1,len(l1t_eta_bounds)):
-        if abs(eta) < l1t_eta_bounds[i]:
-            return i-1
+    for i in xrange(len(l1t_eta_bounds)-1):
+        if l1t_eta_bounds[i] <= abs(eta) < l1t_eta_bounds[i+1]:
+            return i
     return -1
 
 def get_l1t_eta_label(etabin):
@@ -43,16 +52,13 @@ def get_l1t_eta_label(etabin):
     return label
 
 def get_l1t_pu_bin(pu):
-    if pu < l1t_pu_bounds[0] or pu >= l1t_pu_bounds[-1]:
-        return -1
-    for i in xrange(1,len(l1t_pu_bounds)):
-        if pu < l1t_pu_bounds[i]:
-            return i-1
+    for i in xrange(len(l1t_pu_values)):
+        if (l1t_pu_values[i]-1) <= pu < (l1t_pu_values[i]+2):
+            return i
     return -1
 
 def get_l1t_pu_label(pubin):
-    average = lambda x: sum(x) / len(x)
-    label = "# PU = %.0f" % (average([l1t_pu_bounds[pubin], l1t_pu_bounds[pubin+1]]))
+    label = "# PU = %.0f" % (l1t_pu_values[pubin])
     return label
 
 # ______________________________________________________________________________
@@ -66,29 +72,30 @@ def drawer_book(options):
 
     # Bins from Benjamin (Seoul National University) but modified
     #mybins = [0.0, 50.0, 53.0, 55.0, 60.0, 80.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 500.0, 550.0, 600.0, 650.0, 700.0, 800.0, 1000.0]
-    mybins = [0.0, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 12.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 80.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 500.0, 550.0, 600.0, 650.0, 700.0, 800.0, 1000.0]
+    mybins = [0.0, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 12.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 80.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 500.0, 550.0, 600.0, 650.0, 700.0, 800.0, 1000.0, 2000.0]
 
     # Efficiency
     for l1ptbin in l1t_l1pt_vec:
-        for etabin in l1t_eta_vec:
-            hname = "efficiency_of_pt_in_eta%i_l1pt%i" % (etabin, l1ptbin)
-            histos[hname] = TEfficiency(hname, "; gen p_{T} [GeV]; #varepsilon", len(mybins)-1, array('d', mybins))
-            histos[hname].etabin = etabin
+        for modebin in l1t_mode_vec:
+            for etabin in l1t_eta_vec:
+                hname = "efficiency_of_pt_in_eta%i_mode%i_l1pt%i" % (etabin, modebin, l1ptbin)
+                histos[hname] = TEfficiency(hname, "; gen p_{T} [GeV]; #varepsilon", len(mybins)-1, array('d', mybins))
+                histos[hname].etabin = etabin
+                histos[hname].modebin = modebin
+                histos[hname].l1ptbin = l1ptbin
+                hname = "efficiency_of_pu_in_eta%i_mode%i_l1pt%i" % (etabin, modebin, l1ptbin)
+                histos[hname] = TEfficiency(hname, "; gen # PU; #varepsilon", 65, 0, 65)
+                histos[hname].etabin = etabin
+                histos[hname].modebin = modebin
+                histos[hname].l1ptbin = l1ptbin
+            hname = "efficiency_of_eta_mode%i_l1pt%i" % (modebin, l1ptbin)
+            histos[hname] = TEfficiency(hname, "; gen #eta; #varepsilon", 50, -2.5, 2.5)
+            histos[hname].modebin = modebin
             histos[hname].l1ptbin = l1ptbin
-        for pubin in l1t_pu_vec:
-            hname = "efficiency_of_pt_in_pu%i_l1pt%i" % (pubin, l1ptbin)
-            histos[hname] = TEfficiency(hname, "; gen p_{T} [GeV]; #varepsilon", len(mybins)-1, array('d', mybins))
-            histos[hname].pubin = pubin
+            hname = "efficiency_of_phi_mode%i_l1pt%i" % (modebin, l1ptbin)
+            histos[hname] = TEfficiency(hname, "; gen #phi; #varepsilon", 64, -3.2, 3.2)
+            histos[hname].modebin = modebin
             histos[hname].l1ptbin = l1ptbin
-        hname = "efficiency_of_eta_l1pt%i" % (l1ptbin)
-        histos[hname] = TEfficiency(hname, "; gen #eta; #varepsilon", 50, -2.5, 2.5)
-        histos[hname].l1ptbin = l1ptbin
-        hname = "efficiency_of_phi_l1pt%i" % (l1ptbin)
-        histos[hname] = TEfficiency(hname, "; gen #phi; #varepsilon", 64, -3.2, 3.2)
-        histos[hname].l1ptbin = l1ptbin
-        hname = "efficiency_of_pu_l1pt%i" % (l1ptbin)
-        histos[hname] = TEfficiency(hname, "; gen # PU; #varepsilon", 65, 0, 65)
-        histos[hname].l1ptbin = l1ptbin
 
     for pubin in l1t_pu_vec:
         for etabin in l1t_eta_vec:
@@ -159,21 +166,30 @@ def drawer_project(tree, histos, options):
         is_endcap = 1.24 < abs(part_eta) < 2.4
 
         if options.verbose:
-            print ".. %i # PU: %i is endcap: %i" % (ievt, gen_trueNPV, is_endcap)
+            print ".. %i # PU: %i ntracks: %i is_endcap: %i" % (ievt, gen_trueNPV, len(evt.EMTFTracks_pt), is_endcap)
 
         if part_pt > 0 and is_endcap:
             best_mode, best_dR = 0, 0.
             best_trig_pt, best_trig_phi, best_trig_eta = 0., 0., 0.
 
+            trigger_decisions = [[False for modebin in l1t_mode_vec] for l1ptbin in l1t_l1pt_vec]
+
             # Loop over tracks
             for (itrack, mode, trig_pt, trig_phi, trig_eta) in izip(count(), evt.EMTFTracks_mode, evt.EMTFTracks_pt, evt.EMTFTracks_phiGlbRad, evt.EMTFTracks_eta):
 
                 if options.verbose:
-                    print ".. %i gen pt: %f phi: %f eta: %f" % (itrack, part_pt, part_phi, part_eta)
-                    print ".. %i l1t pt: %f phi: %f eta: %f" % (itrack, trig_pt, trig_phi, trig_eta)
+                    print ".... %i gen pt: %f phi: %f eta: %f" % (itrack, part_pt, part_phi, part_eta)
+                    print ".... %i l1t pt: %f phi: %f eta: %f" % (itrack, trig_pt, trig_phi, trig_eta)
 
                 dR = deltaR(trig_eta, trig_phi, part_globalEtaME2, part_globalPhiME2)
 
+                # Get trigger decisions
+                for l1ptbin in l1t_l1pt_vec:
+                    for modebin in l1t_mode_vec:
+                        if (trig_pt > l1t_l1pt_thresholds[l1ptbin]) and (mode in l1t_mode_values[modebin]) and (dR < 0.3):
+                            trigger_decisions[l1ptbin][modebin] = True
+
+                # Get best match
                 if (best_mode < mode):
                     best_mode, best_dR = mode, dR
                     best_trig_pt, best_trig_phi, best_trig_eta = trig_pt, trig_phi, trig_eta
@@ -182,49 +198,54 @@ def drawer_project(tree, histos, options):
                     best_trig_pt, best_trig_phi, best_trig_eta = trig_pt, trig_phi, trig_eta
                 continue
 
-            l1ptbins = get_l1t_l1pt_bins(best_trig_pt)
+            l1ptbins = get_l1t_l1pt_bins()
+            modebins = get_l1t_mode_bins()
             etabin = get_l1t_eta_bin(part_eta)
             pubin = get_l1t_pu_bin(gen_trueNPV)
 
             # Efficiency
             for l1ptbin in l1ptbins:
-                trigger = (best_trig_pt > l1t_l1pt_thresholds[l1ptbin]) and (best_mode in options.modes) and (best_dR < 0.3)
+                for modebin in modebins:
+                    trigger = trigger_decisions[l1ptbin][modebin]
 
-                hname = "efficiency_of_pt_in_eta%i_l1pt%i" % (etabin, l1ptbin)
+                    hname = "efficiency_of_pt_in_eta%i_mode%i_l1pt%i" % (etabin, modebin, l1ptbin)
+                    histos[hname].Fill(trigger, part_pt)
+                    hname = "efficiency_of_pt_in_eta%i_mode%i_l1pt%i" % (l1t_eta_vec[-1], modebin, l1ptbin)  # inclusive eta
+                    histos[hname].Fill(trigger, part_pt)
+                    if part_pt > 20.0:  # cut on denominator
+                        hname = "efficiency_of_pu_in_eta%i_mode%i_l1pt%i" % (etabin, modebin, l1ptbin)
+                        histos[hname].Fill(trigger, gen_trueNPV)
+                        hname = "efficiency_of_pu_in_eta%i_mode%i_l1pt%i" % (l1t_eta_vec[-1], modebin, l1ptbin)  # inclusive eta
+                        histos[hname].Fill(trigger, gen_trueNPV)
+                        hname = "efficiency_of_eta_mode%i_l1pt%i" % (modebin, l1ptbin)
+                        histos[hname].Fill(trigger, part_eta)
+                        hname = "efficiency_of_phi_mode%i_l1pt%i" % (modebin, l1ptbin)
+                        histos[hname].Fill(trigger, part_phi)
+                    continue
+                continue
+
+            trigger = trigger_decisions[-1][-1]  # last l1ptbin, last modebin
+
+            if pubin != -1:
+                hname = "efficiency_of_pt_in_eta%i_pu%i" % (etabin, pubin)
                 histos[hname].Fill(trigger, part_pt)
-                hname = "efficiency_of_pt_in_eta%i_l1pt%i" % (l1t_eta_vec[-1], l1ptbin)  # inclusive
-                histos[hname].Fill(trigger, part_pt)
-                hname = "efficiency_of_pt_in_pu%i_l1pt%i" % (pubin, l1ptbin)
+                hname = "efficiency_of_pt_in_eta%i_pu%i" % (l1t_eta_vec[-1], pubin)  # inclusive eta
                 histos[hname].Fill(trigger, part_pt)
                 if part_pt > 20.0:  # cut on denominator
-                    hname = "efficiency_of_eta_l1pt%i" % (l1ptbin)
+                    hname = "efficiency_of_eta_pu%i" % (pubin)
                     histos[hname].Fill(trigger, part_eta)
-                    hname = "efficiency_of_phi_l1pt%i" % (l1ptbin)
+                    hname = "efficiency_of_phi_pu%i" % (pubin)
                     histos[hname].Fill(trigger, part_phi)
-                    hname = "efficiency_of_pu_l1pt%i" % (l1ptbin)
-                    histos[hname].Fill(trigger, gen_trueNPV)
-
-            trigger = (best_trig_pt > l1t_l1pt_thresholds[-1]) and (best_mode in options.modes) and (best_dR < 0.3)
-
-            hname = "efficiency_of_pt_in_eta%i_pu%i" % (etabin, pubin)
-            histos[hname].Fill(trigger, part_pt)
-            hname = "efficiency_of_pt_in_eta%i_pu%i" % (l1t_eta_vec[-1], pubin)  # inclusive
-            histos[hname].Fill(trigger, part_pt)
-            if part_pt > 20.0:  # cut on denominator
-                hname = "efficiency_of_eta_pu%i" % (pubin)
-                histos[hname].Fill(trigger, part_eta)
-                hname = "efficiency_of_phi_pu%i" % (pubin)
-                histos[hname].Fill(trigger, part_phi)
 
             # Resolution (2D)
             if best_trig_pt > 0:
                 hname = "l1invPt_vs_invPt_in_eta%i" % (etabin)
                 histos[hname].Fill(part_charge/part_pt, 1.0/best_trig_pt)
-                hname = "l1invPt_vs_invPt_in_eta%i" % (l1t_eta_vec[-1])  # inclusive
+                hname = "l1invPt_vs_invPt_in_eta%i" % (l1t_eta_vec[-1])  # inclusive eta
                 histos[hname].Fill(part_charge/part_pt, 1.0/best_trig_pt)
                 hname = "dR_vs_invPt_in_eta%i" % (etabin)
                 histos[hname].Fill(part_charge/part_pt, best_dR)
-                hname = "dR_vs_invPt_in_eta%i" % (l1t_eta_vec[-1])  # inclusive
+                hname = "dR_vs_invPt_in_eta%i" % (l1t_eta_vec[-1])  # inclusive eta
                 histos[hname].Fill(part_charge/part_pt, best_dR)
     return
 
@@ -286,20 +307,30 @@ def drawer_draw_more(histos, options):
     # __________________________________________________________________________
     # Specialized: overlay different pt
     special_hnames = [
-        "efficiency_of_pt_in_eta0",
-        "efficiency_of_pt_in_eta1",
-        "efficiency_of_pt_in_eta2",
-        "efficiency_of_pt_in_eta3",
-        "efficiency_of_pt_in_eta4",
-        "efficiency_of_pt_in_pu0",
-        "efficiency_of_pt_in_pu1",
-        "efficiency_of_pt_in_pu2",
-        "efficiency_of_pt_in_pu3",
-        "efficiency_of_pt_in_pu4",
-        "efficiency_of_pt_in_pu5",
-        "efficiency_of_eta",
-        "efficiency_of_phi",
-        "efficiency_of_pu",
+        "efficiency_of_pt_in_eta0_mode0",
+        "efficiency_of_pt_in_eta0_mode1",
+        "efficiency_of_pt_in_eta0_mode2",
+        "efficiency_of_pt_in_eta1_mode0",
+        "efficiency_of_pt_in_eta1_mode1",
+        "efficiency_of_pt_in_eta1_mode2",
+        "efficiency_of_pt_in_eta2_mode0",
+        "efficiency_of_pt_in_eta2_mode1",
+        "efficiency_of_pt_in_eta2_mode2",
+        "efficiency_of_pu_in_eta0_mode0",
+        "efficiency_of_pu_in_eta0_mode1",
+        "efficiency_of_pu_in_eta0_mode2",
+        "efficiency_of_pu_in_eta1_mode0",
+        "efficiency_of_pu_in_eta1_mode1",
+        "efficiency_of_pu_in_eta1_mode2",
+        "efficiency_of_pu_in_eta2_mode0",
+        "efficiency_of_pu_in_eta2_mode1",
+        "efficiency_of_pu_in_eta2_mode2",
+        "efficiency_of_eta_mode0",
+        "efficiency_of_eta_mode1",
+        "efficiency_of_eta_mode2",
+        "efficiency_of_phi_mode0",
+        "efficiency_of_phi_mode1",
+        "efficiency_of_phi_mode2",
     ]
 
     for special_hname in special_hnames:
@@ -325,10 +356,8 @@ def drawer_draw_more(histos, options):
         tlegend.Draw()
         if hasattr(h, "etabin"):
             tlatex.DrawLatex(0.72, 0.34, get_l1t_eta_label(h.etabin))
-        if hasattr(h, "pubin"):
-            tlatex.DrawLatex(0.72, 0.34, get_l1t_pu_label(h.pubin))
-        if special_hname in ["efficiency_of_eta", "efficiency_of_phi", "efficiency_of_pu"]:
-            tlatex.DrawLatex(0.72, 0.34, "gen p_{T} > 20")
+        if not "efficiency_of_pt" in special_hname:  # cut on denominator
+            tlatex.DrawLatex(0.72, 0.38, "gen p_{T} > 20")
 
         gPad.SetLogx(h.logx); gPad.SetLogy(h.logy)
         gPad.SetGridx(True); gPad.SetGridy(True)
@@ -345,8 +374,6 @@ def drawer_draw_more(histos, options):
         "efficiency_of_pt_in_eta0",
         "efficiency_of_pt_in_eta1",
         "efficiency_of_pt_in_eta2",
-        "efficiency_of_pt_in_eta3",
-        "efficiency_of_pt_in_eta4",
         "efficiency_of_eta",
         "efficiency_of_phi",
     ]
@@ -361,7 +388,7 @@ def drawer_draw_more(histos, options):
         h1.SetMinimum(0); h1.SetMaximum(ymax)
         h1.SetStats(0); h1.Draw()
 
-        moveLegend(tlegend,0.66,0.14,0.90,0.32); tlegend.Clear()
+        moveLegend(tlegend,0.66,0.16,0.90,0.32); tlegend.Clear()
         for i, pubin in enumerate(l1t_pu_vec):
             suffix = "_pu%i" % (pubin)
             hname = special_hname + suffix
@@ -374,8 +401,8 @@ def drawer_draw_more(histos, options):
         tlegend.Draw()
         if hasattr(h, "etabin"):
             tlatex.DrawLatex(0.72, 0.34, get_l1t_eta_label(h.etabin))
-        if special_hname in ["efficiency_of_eta", "efficiency_of_phi"]:
-            tlatex.DrawLatex(0.72, 0.34, "gen p_{T} > 20")
+        if not "efficiency_of_pt" in special_hname:  # cut on denominator
+            tlatex.DrawLatex(0.72, 0.38, "gen p_{T} > 20")
 
         gPad.SetLogx(h.logx); gPad.SetLogy(h.logy)
         gPad.SetGridx(True); gPad.SetGridy(True)
@@ -384,7 +411,7 @@ def drawer_draw_more(histos, options):
 
         if "efficiency_of_pt" in special_hname:
             h1.GetXaxis().SetRangeUser(0, 200)
-            save(options.outdir, hname[:hname.find("_l1pt")]+"_l1pt_zoom")
+            save(options.outdir, hname[:hname.find("_pu")]+"_pu_zoom")
     return
 
 # ______________________________________________________________________________
@@ -392,10 +419,11 @@ def drawer_sitrep(histos, options):
     print "--- SITREP --------------------------------------------------------"
 
     for l1ptbin in l1t_l1pt_vec:
-        for etabin in l1t_eta_vec:
-            hname = "efficiency_of_pt_in_eta%i_l1pt%i" % (etabin, l1ptbin)
-            h = histos[hname]
-            print hname, h.GetCopyTotalHisto().GetEntries()
+        for modebin in l1t_mode_vec:
+            for etabin in l1t_eta_vec:
+                hname = "efficiency_of_pt_in_eta%i_mode%i_l1pt%i" % (etabin, modebin, l1ptbin)
+                h = histos[hname]
+                print hname, h.GetCopyTotalHisto().GetEntries()
 
 
 # ______________________________________________________________________________
@@ -418,10 +446,7 @@ if __name__ == '__main__':
 
     # Setup argument parser
     parser = MyParser()
-    parser.add_argument("--modes", default="11,13,14,15", type=str, help="EMTF track modes")
     options = parser.parse_args()
-    options.modes = [int(mode) for mode in options.modes.split(',')]
-    print "use modes: ", options.modes
 
     # Call the main function
     main(options)
